b59a43d1aca9d64919b43d80d1caca256cbe0cce
Noah <practicasjaguar@gmail.com>
2024-03-28 11:09:02 -0500

    tipos datos basicos

---
+++ tiposdatos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/* Tipos de datos */
+
+int main() {
+	int entero = 10;
+	float decimal = 10.5;
+	double mayor = 1045.423;
+	
+	cout << entero <<endl;
+	cout << decimal <<endl;
+	cout << mayor <<endl;
+	return 0;
+}
