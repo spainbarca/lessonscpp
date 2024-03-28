529fae323256cb50d5d7453d16481a5019495f85
Noah <practicasjaguar@gmail.com>
2024-03-28 11:24:33 -0500

    lectura datos

---
+++ lectura.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* lectura o entrada de datos */
+
+int main() {
+	int numero; //definir la variable
+	cout << "Ingrese el numero: "; //solicitar el valor
+	cin >> numero; // asignar el valor a la variable
+	
+	cout << "El numero digitado es: " << numero; //mostrar el valor
+
+	return 0;
+}
